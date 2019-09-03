d700525bf752a47a29d2cc466b932cb8616e4634
Jason Zhang <totaljfb@gmail.com>
2019-09-03 17:40:20 -0400

    single floating action button design and implementation completed

---
+++ app/src/main/java/com/example/viewanimation/FloatingButtonStatus.java
@@ -0,0 +1,5 @@
+package com.example.viewanimation;
+
+public enum FloatingButtonStatus {
+    Initiate, Navigate, Stop
+}
--- app/src/main/java/com/example/viewanimation/MainActivity.java
+++ app/src/main/java/com/example/viewanimation/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.viewanimation;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import static com.example.viewanimation.FloatingButtonStatus.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -20,8 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private View view2;
     private View view3;
     private FloatingActionButton fab;
-    private ObjectAnimator objectAnimator;
-    private FloatingActionButton fab2;
+    private FloatingButtonStatus fabstatus = Initiate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +26,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         fab = findViewById(R.id.floatingActionButton2);
         fab.setOnClickListener(this);
-        fab2 = findViewById(R.id.floatingActionButton3);
-        fab2.setOnClickListener(this);
         view1 = findViewById(R.id.imageView1);
         view2 = findViewById(R.id.imageView2);
         view3 = findViewById(R.id.imageView3);
@@ -41,50 +35,86 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v){
         int h = 0;
         ViewGroup.LayoutParams layoutParams = null;
+        ObjectAnimator objectAnimator = null;
+        ValueAnimator va = null;
+        int mDuration = 500;
         switch (v.getId()){
             case R.id.floatingActionButton2:
-                h = view2.getHeight();
-                layoutParams = view3.getLayoutParams();
-                layoutParams.height = h/2;
-                view3.setLayoutParams(layoutParams);
-                objectAnimator = ObjectAnimator.ofFloat(view3, "y", 2*h, h/2);
-                objectAnimator.setDuration(500);
-                objectAnimator.start();
+                switch (fabstatus){
+                    case Initiate:
+                        h = view2.getHeight();
+                        layoutParams = view3.getLayoutParams();
+                        layoutParams.height = h/2;
+                        view3.setLayoutParams(layoutParams);
+                        objectAnimator = ObjectAnimator.ofFloat(view3, "y", h, h/2);
+                        objectAnimator.setDuration(500);
+                        objectAnimator.start();
 
-                ValueAnimator va = ValueAnimator.ofInt(h,h/2);
-                int mDuration = 500; //in millis
-                va.setDuration(mDuration);
-                va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    public void onAnimationUpdate(ValueAnimator animation) {
-                        int val = (Integer) animation.getAnimatedValue();
-                        ViewGroup.LayoutParams layoutParams = view2.getLayoutParams();
-                        layoutParams.height = val;
-                        view2.setLayoutParams(layoutParams);
-                    }
-                });
-                va.start();
+                        va = ValueAnimator.ofInt(h,h/2);
+                        va.setDuration(mDuration);
+                        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                int val = (Integer) animation.getAnimatedValue();
+                                ViewGroup.LayoutParams layoutParams = view2.getLayoutParams();
+                                layoutParams.height = val;
+                                view2.setLayoutParams(layoutParams);
+                            }
+                        });
+                        va.start();
 
-                objectAnimator = ObjectAnimator.ofFloat(view2, "y", 0, 0);
-                objectAnimator.setDuration(500);
-                objectAnimator.start();
-                break;
-            case R.id.floatingActionButton3:
-                h = view2.getHeight();
+                        objectAnimator = ObjectAnimator.ofFloat(view2, "y", 0, 0);
+                        objectAnimator.setDuration(500);
+                        objectAnimator.start();
+                        fabstatus = Navigate;
+                        fab.setImageResource(android.R.drawable.ic_menu_directions);
+                        break;
+                    case Navigate:
+                        h = view2.getHeight();
+                        objectAnimator = ObjectAnimator.ofFloat(view3, "y", h, 2*h);
+                        objectAnimator.setDuration(500);
+                        objectAnimator.start();
 
-                objectAnimator = ObjectAnimator.ofFloat(view3, "y", h, 2*h);
-                objectAnimator.setDuration(500);
-                objectAnimator.start();
+                        objectAnimator = ObjectAnimator.ofFloat(view2, "y", 0, h);
+                        objectAnimator.setDuration(500);
+                        objectAnimator.start();
 
-                objectAnimator = ObjectAnimator.ofFloat(view2, "y", 0, h);
-                objectAnimator.setDuration(500);
-                objectAnimator.start();
+                        layoutParams = view1.getLayoutParams();
+                        layoutParams.height = h;
+                        view1.setLayoutParams(layoutParams);
+                        objectAnimator = ObjectAnimator.ofFloat(view1, "y", -h, 0);
+                        objectAnimator.setDuration(500);
+                        objectAnimator.start();
+                        fabstatus = Stop;
+                        fab.setImageResource(android.R.drawable.presence_busy);
+                        break;
+                    case Stop:
+                        h = view1.getHeight();
+                        objectAnimator = ObjectAnimator.ofFloat(view1, "y", 0, -h);
+                        objectAnimator.setDuration(500);
+                        objectAnimator.start();
 
-                layoutParams = view1.getLayoutParams();
-                layoutParams.height = h;
-                view1.setLayoutParams(layoutParams);
-                objectAnimator = ObjectAnimator.ofFloat(view1, "y", -h, 0);
-                objectAnimator.setDuration(500);
-                objectAnimator.start();
+                        va = ValueAnimator.ofInt(h,2*h);
+                        va.setDuration(mDuration);
+                        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                int val = (Integer) animation.getAnimatedValue();
+                                ViewGroup.LayoutParams layoutParams = view2.getLayoutParams();
+                                layoutParams.height = val;
+                                view2.setLayoutParams(layoutParams);
+                            }
+                        });
+                        va.start();
+
+                        objectAnimator = ObjectAnimator.ofFloat(view2, "y", 2*h, 0);
+                        objectAnimator.setDuration(500);
+                        objectAnimator.start();
+                        fabstatus = Initiate;
+                        fab.setImageResource(android.R.drawable.ic_menu_search);
+                        break;
+                    default:
+                        break;
+                }
+
             default:
                 break;
         }
